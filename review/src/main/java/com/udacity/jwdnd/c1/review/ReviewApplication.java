a51a8a3b9b379ccadfd625732695408626b16cb0
nari-suke <goodboop86@gmail.com>
2020-10-24 16:58:00 +0900

    Update .gitignore (add .DS_Store)

--- review/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ review/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -1,13 +1,39 @@
 package com.udacity.jwdnd.c1.review;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
-public class ReviewApplication {
+@Configuration
+@EnableAutoConfiguration
+public class    ReviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
+
+	@Primary
+	@Bean
+	public String basicMessage(){
+		System.out.println("inside basicMessage");
+		return "Hello";
+	}
+
+	@Bean
+	public String compoundMessage(String basicMessage){
+		System.out.println("inside compoundMessage, received:" + basicMessage);
+		return basicMessage + ", Spring!";
+	}
+
+
+	@Bean
+	public int characterCount(String message){ //recieves compound message because of the @Primary annotation
+		return message.length();
+	}
+
 
 }
