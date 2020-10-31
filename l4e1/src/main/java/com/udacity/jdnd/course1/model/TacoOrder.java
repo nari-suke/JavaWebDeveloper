6663694c62c764c457816b336f0765712b9096d0
nari-suke <goodboop86@gmail.com>
2020-10-31 19:33:23 +0900

    add l4e1

---
+++ l4e1/src/main/java/com/udacity/jdnd/course1/model/TacoOrder.java
@@ -0,0 +1,10 @@
+package com.udacity.jdnd.course1.model;
+
+public class TacoOrder {
+    private Integer orderId;
+    private String tacoName;
+
+    private Double tacoPrice;
+    private Integer count;
+
+}
