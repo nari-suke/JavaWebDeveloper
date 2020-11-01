2448500150c8f8cfb063d0e6b0d60b425c21979d
nari-suke <goodboop86@gmail.com>
2020-11-01 19:15:29 +0900

    add SignupController (dependency by mmodel.User, service.UserService)

---
+++ review/src/main/java/com/udacity/jwdnd/c1/review/controller/SignupController.java
@@ -0,0 +1,45 @@
+package com.udacity.jwdnd.c1.review.controller;
+
+import com.udacity.jwdnd.c1.review.model.User;
+import com.udacity.jwdnd.c1.review.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+
+@Controller()
+@RequestMapping("/signup")
+public class SignupController {
+
+    private final UserService useService;
+
+    public SignupController(UserService useService) {
+        this.useService = useService;
+    }
+
+    @GetMapping()
+    public String signupView() {
+        return "signup";
+    }
+
+    @PostMapping()
+    public String signupUser(@ModelAttribute User user, Model model) {
+        String signupError = null;
+
+        if (!useService.isUsernameAvailable(user.getUsername())) {
+            signupError = "There was an error signing you up. Please try again.";
+        }
+
+        if (signupError == null) {
+            model.addAttribute("signupSuccess", true);
+        } else {
+            model.addAttribute("signupError", signupError);
+        }
+
+        return "signup";
+    }
+}
