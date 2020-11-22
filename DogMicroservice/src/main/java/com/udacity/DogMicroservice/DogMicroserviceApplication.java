ce9b7b7dfc2c3ea5ab32ca23742a034989d1f778
nari-suke <goodboop86@gmail.com>
2020-11-23 00:13:48 +0900

    add DogMicroService (in section3)

--- DogMicroservice/src/main/java/com/udacity/DogMicroservice/DogMicroserviceApplication.java
+++ DogMicroservice/src/main/java/com/udacity/DogMicroservice/DogMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.udacity.DogMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DogMicroserviceApplication {
 
 	public static void main(String[] args) {
