49ce429bd88df5af4898a27bf0ab69463e85b44d
nari-suke <goodboop86@gmail.com>
2020-12-20 17:44:21 +0900

    Done exercise3 (PlantRepository is poorly implemented)

--- course3/src/main/java/com/udacity/jdnd/course3/repository/PlantRepository.java
+++ course3/src/main/java/com/udacity/jdnd/course3/repository/PlantRepository.java
@@ -1,14 +1,17 @@
 package com.udacity.jdnd.course3.repository;
 
 import com.udacity.jdnd.course3.data.Plant;
+import org.hibernate.type.TrueFalseType;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public class PlantRepository {
-    public List<Plant> findByPriceLessThan(BigDecimal valueOf) {
+    public List<Plant> findByPriceLessThan(BigDecimal num) {
+        return (List<Plant>) num;
     }
 
     public boolean deliveryCompleted(Long id) {
+        return true;
     }
 }
