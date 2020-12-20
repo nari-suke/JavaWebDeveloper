5630e9b8c0424c10afa43501bb7d93b17fd188ec
nari-suke <goodboop86@gmail.com>
2020-12-20 17:47:26 +0900

    Revert "Done exercise3 (PlantRepository is poorly implemented)"
    This reverts commit 49ce429bd88df5af4898a27bf0ab69463e85b44d.

--- course3/src/main/java/com/udacity/jdnd/course3/repository/PlantRepository.java
+++ course3/src/main/java/com/udacity/jdnd/course3/repository/PlantRepository.java
@@ -1,17 +1,14 @@
 package com.udacity.jdnd.course3.repository;
 
 import com.udacity.jdnd.course3.data.Plant;
-import org.hibernate.type.TrueFalseType;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public class PlantRepository {
-    public List<Plant> findByPriceLessThan(BigDecimal num) {
-        return (List<Plant>) num;
+    public List<Plant> findByPriceLessThan(BigDecimal valueOf) {
     }
 
     public boolean deliveryCompleted(Long id) {
-        return true;
     }
 }
