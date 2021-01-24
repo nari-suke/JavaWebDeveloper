d6d6fefd92dbb6c864f3213bc27970f4b0380dd4
nari-suke <goodboop86@gmail.com>
2021-01-24 21:03:57 +0900

    add naming module (referenced by https://howtodoinjava.com/spring-cloud/spring-cloud-service-discovery-netflix-eureka/)

---
+++ P02-VehiclesAPI/naming/src/main/java/com/udacity/naming/NamingApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.naming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class NamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingApplication.class, args);
+	}
+
+}
---
+++ P02-VehiclesAPI/naming/src/test/java/com/udacity/naming/NamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.naming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
